d220f63041b4ccdf3ce76543be2c467a1f26bb16
ACER <210220@softwarica.edu.np>
2025-01-03 23:22:44 +0545

    services '

---
+++ app/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Layout from './_layout';
+
+const App = () => (
+<NavigationContainer>
+    <Layout />
+</NavigationContainer>
+);
+
+export default App;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,13 @@
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen'
 
 
 const Stack = createStackNavigator();
 
-const App = () => (
-<NavigationContainer>
+const Layout = () => (
     <Stack.Navigator>
     <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
-</NavigationContainer>
 );
+
+export default Layout;
