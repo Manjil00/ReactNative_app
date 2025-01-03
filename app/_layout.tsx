a6342b5bcd0d36ae06e47b97fe9a00e875b3bc20
ACER <210220@softwarica.edu.np>
2025-01-03 22:10:09 +0545

    layout

---
+++ app/_layout.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function App() {
+return (
+    <View style={styles.container}>
+    <Text> Hello, NativeWind! wassup cuh</Text>
+    </View>
+);
+}
+
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor:'#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
